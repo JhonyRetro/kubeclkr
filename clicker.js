ebf649702865d8b475cecd5153f799ab3936ffd3
JhonyRetro <jhonan989@gmail.com>
2026-04-29 15:11:58 +0200

    first commit

---
+++ clicker.js
@@ -0,0 +1,17 @@
+let count = localStorage.getItem('clicks') ? parseInt(localStorage.getItem('clicks')) : 0;
+
+const button = document.getElementById('red-button');
+const display = document.getElementById('count');
+
+display.innerText = count;
+
+button.addEventListener('click', () => {
+    count++;
+    display.innerText = count;
+
+    if (count === 100) {
+        document.body.style.backgroundColor = "#4a0000";
+        document.getElementById('counter-container').style.color = "#ff4d4d";
+        alert("Muchos clicks eh...");
+    }
+});
